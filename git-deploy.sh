f8bd05153d288527d30a6b68dd10dc91432ee985
ahmet-ari <batmanahmet1@gmail.com>
2021-05-06 16:02:12 +0000

    Testi

--- git-deploy.sh
+++ git-deploy.sh
@@ -5,6 +5,6 @@ do
     git init
     git add .
     git commit -m "Testi"
-    git remote add origin https://github.com/ahmet-ari/ahmetari_swd
+    git remote add origin https://github.com/hamk-linux-20a6/ahmetari_assignments
     git push -u origin master
 done
